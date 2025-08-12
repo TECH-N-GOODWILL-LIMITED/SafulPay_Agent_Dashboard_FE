20ddc476ac5c184846bb9f2d079a54c8a668d0a3
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2025-08-12 19:40:44 +0000

    fix: vite config error fix

--- vite.config.ts
+++ vite.config.ts
@@ -22,21 +22,6 @@ export default defineConfig({
         target: "https://ycd141j4sl.execute-api.us-east-1.amazonaws.com/v1",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
-        configure: (proxy, options) => {
-          proxy.on("error", (err, req, res) => {
-            console.log("proxy error", err);
-          });
-          proxy.on("proxyReq", (proxyReq, req, res) => {
-            console.log("Sending Request to the Target:", req.method, req.url);
-          });
-          proxy.on("proxyRes", (proxyRes, req, res) => {
-            console.log(
-              "Received Response from the Target:",
-              proxyRes.statusCode,
-              req.url
-            );
-          });
-        },
       },
     },
   },
